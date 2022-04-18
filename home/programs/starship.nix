80f7073f966d42e87ff707d70918dfd14787639f
GiyoMoon <giyomoon@gmail.com>
2022-04-18 10:16:04 +0200

    :wrench: Remove starship format for now

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -4,7 +4,8 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$battery$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+      # TODO: Find a cool, fitting format
+      # format = "";
     };
   };
 }
