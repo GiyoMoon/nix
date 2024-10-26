3cdd54ba1cea12ef5dddd36c4b76ae9463a29405
Jasmin <code@jasi.dev>
2024-10-26 20:00:23 +0200

    feat: disable treesitter for long lines

--- home/programs/nixvim/config/plugins/treesitter.nix
+++ home/programs/nixvim/config/plugins/treesitter.nix
@@ -4,7 +4,24 @@
       treesitter = {
         enable = true;
         settings = {
-          highlight.enable = true;
+          highlight = {
+            enable = true;
+            disable.__raw = # lua
+              ''
+                function(lang, buf)
+                  local max_char_count = 100000
+                  local line_count = vim.api.nvim_buf_line_count(buf)
+                  if line_count == 1 then
+                    local line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+                      if #line > max_line_length then
+                        return true
+                      else
+                        return false
+                      end
+                    end
+                  end
+              '';
+          };
           indent.enable = true;
         };
         languageRegister = {
