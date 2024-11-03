c25c1736e27c7e3e0346b88515bf3364d7bd6d90
Jasmin <code@jasi.dev>
2024-11-03 14:36:21 +0100

    fix: telescope ignore files

--- home/programs/nixvim/config/plugins/noice.nix
+++ home/programs/nixvim/config/plugins/noice.nix
@@ -6,21 +6,23 @@
     };
     noice = {
       enable = true;
-      presets = {
-        bottom_search = true;
-        command_palette = true;
-        long_message_to_split = true;
-        inc_rename = false;
-        lsp_doc_border = false;
-      };
-      lsp.override = {
-        "vim.lsp.util.convert_input_to_markdown_lines" = true;
-        "vim.lsp.util.stylize_markdown" = true;
-      };
-      messages = {
-        enabled = true;
-        view = "mini";
-        viewError = "notify";
+      settings = {
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+          inc_rename = false;
+          lsp_doc_border = false;
+        };
+        lsp.override = {
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+        };
+        messages = {
+          enabled = true;
+          view = "mini";
+          viewError = "notify";
+        };
       };
     };
   };
